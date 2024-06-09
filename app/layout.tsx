d3d1c81ff9889e28da1d75b252476a84137f4227
Farhan Masood <farhan.masood@mslm.io>
2024-06-09 11:01:01 +0500

    initializing next app, convex setup, components, shadcn, schema

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import ConvexClientProvider from './providers/ConvexProvider';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Podcastr',
+  description: 'Generate your podcast using AI',
+  icons: {
+    icon: '/icons/logo.svg',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>{' '}
+      </body>
+    </html>
+  );
+}
